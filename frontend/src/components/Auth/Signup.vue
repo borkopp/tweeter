b25c097c04e73554af1f6dcfcd3ae84cfb5bb8a4
Borko Petrevski <borko.petrevski@hs-augsburg.de>
2024-06-15 19:59:42 +0200

    added docker for frontend/compose, added username and name input on signup page

--- frontend/src/components/Auth/Signup.vue
+++ frontend/src/components/Auth/Signup.vue
@@ -5,6 +5,8 @@
         <h2>Sign Up</h2>
         <form @submit.prevent="signup">
             <input type="email" v-model="email" placeholder="Email" required />
+            <input type="text" v-model="username" placeholder="Username" required />
+            <input type="text" v-model="name" placeholder="Name" required />
             <input type="password" v-model="password" placeholder="Password" required />
             <button type="submit">Sign Up</button>
         </form>
